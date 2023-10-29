201c7f8f14a2295b48606a20540714d139341578
Samer  El-Murr <samer@trinity.co>
2023-10-29 16:41:03 +0200

    Added bounce animation for search bar

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -8,7 +8,7 @@ export const SearchBox = () => {
       <input 
         type="search" 
         id="default-search" 
-        className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+        className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 animate-[bounce_500ms_ease-in-out_infinite]" 
         placeholder="Search"
         value={search}
         onChange={(e) => setSearch(e.target.value)}
